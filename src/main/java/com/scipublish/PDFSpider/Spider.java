1c6e760de1c0e9013ee28bacced0996f462fc6d5
ZhouQiang <zhouqiang@gozap.com>
2013-07-03 18:33:11 +0800

    base commit

---
+++ src/main/java/com/scipublish/PDFSpider/Spider.java
@@ -0,0 +1,15 @@
+package com.scipublish.PDFSpider;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chouchris
+ * Date: 13-7-3
+ * Time: PM6:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class Spider {
+
+    public static void main(String[] args){
+
+    }
+}
